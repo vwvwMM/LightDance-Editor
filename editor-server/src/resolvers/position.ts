e19bab4ace7e9d64e0ecbcf9f99dc3105c778297
Alanhsiu <alan901101@gmail.com>
2023-01-31 01:21:30 +0800

    four files

--- editor-server/src/resolvers/position.ts
+++ editor-server/src/resolvers/position.ts
@@ -9,3 +9,17 @@ export class PositionResolver {
     return data;
   }
 }
+// import { Resolver, FieldResolver, Root, Ctx } from "type-graphql";
+// import { IPosition, TContext } from "../types/global";
+// import { PositionData } from "../../prisma/generated/type-graphql";
+
+// @Resolver((of) => PositionData)
+// export class PositionResolver {
+//   @FieldResolver(() => PositionData)
+//   async frame(@Root() position: IPosition, @Ctx() ctx: TContext) {
+//     const data = ctx.prisma.positionFrame.findFirst({
+//       where: { id: position.frame },
+//     });
+//     return data;
+//   }
+// }
